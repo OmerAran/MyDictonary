104158f14883ac41074a0ea390348a98b12d2a28
Ömer Aran <omeraranofficial@gmail.com>
2021-01-28 09:55:26 +0300

    Add project files.

---
+++ MyDictonary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyDictonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictonary<int, string> myDictonary = new MyDictonary<int, string>();
+            myDictonary.Add(1, "A");
+            myDictonary.Add(2, "B");
+            myDictonary.Add(3, "C");
+            myDictonary.Add(4, "D");
+            myDictonary.Add(5, "E");
+            myDictonary.Add(6, "F");
+            myDictonary.Add(7, "G");
+            myDictonary.Add(8, "H");
+            myDictonary.Add(9, "I");
+
+            myDictonary.Show();
+
+        }
+    }
+}
